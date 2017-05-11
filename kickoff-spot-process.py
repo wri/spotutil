b563e703b8cc0edad57611caac4a4787daa7e89b
sgibbes <sgibbes@wri.org>
2017-05-11 08:33:29 -0400

    Updated ami ID to use new aws creds and python requests

--- kickoff-spot-process.py
+++ kickoff-spot-process.py
@@ -9,7 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description='Start spot instance and run multiprocessing jobs')
     
     # Args related to the machine we'll be starting
-    parser.add_argument('--ami-id', '-a', default='ami-16bd3500', help="ID of the AMI you want to launch")
+    parser.add_argument('--ami-id', '-a', default='ami-40275656', help="ID of the AMI you want to launch")
     parser.add_argument('--instance-type', '-i', required=True, help='spot instance type')
     parser.add_argument('--price', default='3.00', help='price for each instance')
     parser.add_argument('--disk-size', '-d', default=50, help='disk size in GB')
